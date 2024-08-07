e8dec9b64cd24367223808109cb1fa34d7b82b61
Masafumi Higashi <higashi-masafumi507@g.ecc.u-tokyo.ac.jp>
2024-08-07 15:52:12 +0900

    googleでのログインのためのリダイレクトファイル他を追加

---
+++ app/routes/auth.callback.tsx
@@ -0,0 +1,51 @@
+import { redirect, type LoaderFunctionArgs } from '@remix-run/node'
+import { createServerClient, parseCookieHeader, serializeCookieHeader } from '@supabase/ssr'
+import { signUp } from '~/db/server.user'
+import { getSession, commitSession } from '~/sessions'
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
+  const next = requestUrl.searchParams.get('next') || '/'
+  const headers = new Headers()
+
+  if (code) {
+    const supabase = createServerClient(process.env.VITE_SUPABASE_URL!, process.env.VITE_SUPABASE_ANON_KEY!, {
+      cookies: {
+        getAll() {
+          return parseCookieHeader(request.headers.get('Cookie') ?? '')
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            headers.append('Set-Cookie', serializeCookieHeader(name, value, options))
+          )
+        },
+      },
+    })
+
+    const { error } = await supabase.auth.exchangeCodeForSession(code)
+
+    if (!error) {
+        const { data: { user } } = await supabase.auth.getUser()
+        if(user) {
+            // 新規ユーザー登録
+            const id = user.id as string;
+            const email = user.email as string;
+            const name = user.user_metadata?.full_name as string;
+            const imageurl = user.user_metadata?.avatar_url as string;
+            const newuser = await signUp(id, email, name, imageurl);
+            if (newuser) {
+                const session = await getSession(request.headers.get('Cookie'));
+                session.set('userId', id);
+                return redirect("/", {
+                    headers: {
+                        "Set-Cookie": await commitSession(session),
+                }
+            });
+            }
+        }
+    }
+  }
+  // return the user to an error page with instructions
+  throw new Response('Error signing in', { status: 500 })
+}
---
+++ prisma/migrations/20240806070454_init/migration.sql
@@ -0,0 +1,48 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "auth";
+
+-- CreateTable
+CREATE TABLE "auth"."User" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(30) NOT NULL DEFAULT 'anoymous',
+    "bio" TEXT,
+    "email" TEXT NOT NULL,
+    "imageurl" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Events" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(50) NOT NULL DEFAULT '無名のイベント',
+    "description" TEXT,
+    "holderId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "candidates" TIMESTAMP(3)[],
+
+    CONSTRAINT "Events_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Participants" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "abscence" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "remarks" TEXT DEFAULT '備考なし',
+    "eventId" INTEGER NOT NULL,
+
+    CONSTRAINT "Participants_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "auth"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."Events" ADD CONSTRAINT "Events_holderId_fkey" FOREIGN KEY ("holderId") REFERENCES "auth"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Participants" ADD CONSTRAINT "Participants_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "public"."Events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Participants" ADD CONSTRAINT "Participants_userId_fkey" FOREIGN KEY ("userId") REFERENCES "auth"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
